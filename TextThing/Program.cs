86f80b18ebb1e5158bbc4374184067fbea4fd811
elennox <elennox191@gmail.com>
2015-10-23 12:35:02 -0400

    Initial commit

---
+++ TextThing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if(input.Contains("new"))
+            {
+                var game = new TextGame();
+            }
+        }
+    }
+}
---
+++ TextThing/TextGame.cs
@@ -0,0 +1,14 @@
+﻿namespace TextThing
+{
+    internal class TextGame
+    {
+        public string Name;
+        
+
+        public TextGame()
+        {
+
+
+        }  
+    }
+} 
